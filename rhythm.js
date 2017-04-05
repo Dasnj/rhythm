48b785cb5ddb8d97569ee57eea20fe17c51bb7f4
ducongshi <congshi.du@ele.com>
2017-04-05 19:18:56 +0800

    主体结构

--- rhythm.js
+++ rhythm.js
@@ -1,54 +1,90 @@
-(function(window, document, undefined){
+(function(root, factory){
+	if(typeof define === "function" && define.amd){
+		define(factory);
+	}else if(typeof exports === "object"){
+		module.exports = factory();
+	}else{
+		root.ry = factory();
+	}
+}(this, function(){
+	'use strict';
+	var rJS, rJSON, rQ, rQuery, Rhythm, RhythmEvent, ry, tagPrefix = 'ry';
 
-    'use strict';
+	////////////////
+	// 全局方法集合 //
+	////////////////
 
-    var rJS, rJSON, rQ, Rhythm, ry, tagPrefix = 'ry';
+    function func(){
+        //
+    }
 
-    // 定义事件发射器
-    var RhythmEvent = function(){};
-    RhythmEvent.prototype.constructor = RhythmEvent;
+	rJS = {};
 
-    Rhythm = function(){};
-    // Rhythm 继承 RhythmEvent
-    Rhythm.prototype = Object.create(RhythmEvent.prototype);
-    Rhythm.prototype.constructor = Rhythm;
+	rJSON = {};
 
-    // 内置JS方法
-    rJS = {};
+	/////////////////
+	// 简易化jQuery //
+	/////////////////
 
-    // JSON
-    rJSON = {};
-
-    // 类似jQuery的DOM操作
-    var rQuery = function(){};
-
-    ry = new Rhythm();
-    rQ = new rQuery();
-    ry.rQ = rQ;
-
-    var eventDOMChange = function(){};
-    var eventInputChange = function(e){};
-
-
-    // 定义侦听事件目录
-    var setEventListener = function(){
-        rQ.dom('body').on('DOMSubtreeModified', eventDOMChange);
-        rQ.dom('[' + tagPrefix + '-data]').on('input change', eventInputChange);
+	rQuery = function(){};
+	rQuery.constructor = rQuery;
+    rQuery.prototype.ready = function(callback){
+        //
     };
 
-    // 设定初始化并于HTML加载完成后执行
-    var eventInit = function(){
-        setEventListener(); // 全局侦听
-        // ry.restore(); // 本地存储恢复数据
-        // ry.setDefaults(); // 设置默认值
-        // ry.registerDependencies(); // 寄存器依赖关系
-        // ry.updateDependencies(); // 更新依赖关系
-    };
+	rQ = new rQuery();
 
-    rQ.ready(eventInit);
 
-    window.ry = ry;
+	////////////////
+	// 全局主体集合 //
+	////////////////
 
-    return ry;
+	RhythmEvent = function(){};
+	RhythmEvent.prototype.constructor = RhythmEvent;
 
-}(window, document));
+	Rhythm = function(){
+		this.data = {}; // 页面总数据
+		this._binding = {}; // 表单中所有需要侦听的元素
+		this.option = {
+			persistent: false,
+			timeoutInput: 50,
+			timeoutDOM: 500
+		};
+	};
+	Rhythm.prototype = Object.create(RhythmEvent.prototype);
+	Rhythm.constructor = Rhythm;
+
+	ry = new Rhythm();
+	ry.rQ = rQ;
+
+
+	////////////////
+	// 组织全局逻辑 //
+	////////////////
+
+
+	var timeoutInput = null, timeoutDOM = null;
+	var eventDOMChange = function(){
+		//
+	};
+	var eventInputChange = function(){
+		//
+	};
+	// 事件侦听列表
+	var setEventListeners = function(){
+		rQ.dom('body').on('DOMSubtreeModified', eventDOMChange);
+		rQ.dom('[' + tagPrefix + '-data]').on('input change', eventInputChange);
+	};
+
+	var eventInit = function(){
+		setEventListeners();
+		ry.restore();
+		ry.setDefaults();
+		ry.registerDependencies();
+		ry.updateDependencies();
+	};
+
+	rQ.ready(eventInit);
+
+	return ry;
+}));
